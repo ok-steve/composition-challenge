9320b2b012f22c1bc61b83cf7f8012a822e84c49
Steve Cherry <steve@stevecherry.net>
2023-03-14 15:44:17 -0400

    Update deploy action

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,7 @@
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
 const eleventyRssPlugin = require('@11ty/eleventy-plugin-rss');
-const pkg = require('./package.json');
 
 module.exports = function (eleventyConfig) {
-  const pathPrefix =
-    process.env.ELEVENTY_RUN_MODE === 'build' ? `/${pkg.name}/` : '/';
-
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPlugin(eleventyRssPlugin);
 
@@ -17,7 +13,6 @@ module.exports = function (eleventyConfig) {
   });
 
   return {
-    pathPrefix,
     htmlTemplateEngine: 'njk',
     markdownTemplateEngine: 'njk',
     dir: {
